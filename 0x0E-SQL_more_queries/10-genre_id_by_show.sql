4cb20b1ed8c99e73f314b9200f15f7ec3c4ce82f
Praisedoumar <Adigunpraiseoluwafikayo@gmail.com>
2023-04-26 17:37:01 -0700

    Updated

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++
@@ -1,7 +0,0 @@
--- List shows with genre
-SELECT tv_shows.title, tv_show_genres.genre_id 
-	FROM tv_genres
-	JOIN tv_show_genres
-	JOIN tv_shows
-	ON tv_shows.id = show_id AND tv_genres.id = genre_id
-	ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
